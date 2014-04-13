f3716f8591358421fdcfbfffe17c5ef8bfec48eb
Alex Rodionov <p0deje@gmail.com>
2014-04-13 20:57:50 +0700

    fix uninitialized constant Watir::Timecop

--- lib/watir-timecop/timer.rb
+++ lib/watir-timecop/timer.rb
@@ -1,25 +1,29 @@
 require 'timeout'
 
-class Watir::Timecop::Timer
+module Watir
+  module Timecop
+    class Timer
 
-  #
-  # Executes given block until it returns true or exceeds timeout.
-  # It is different from default Watir::Wait::Timer implementation
-  # since we use `Timeout.timeour` rather than `Time.now` to determine if
-  # waiting has exceeded timeout. Usage of `Time.now` is not compatible
-  # with Timecop gem - we may never exceed the timeout if it's stubbed.
-  #
-  # @param [Fixnum] timeout
-  # @yield block
-  # @api private
-  #
+      #
+      # Executes given block until it returns true or exceeds timeout.
+      # It is different from default Watir::Wait::Timer implementation
+      # since we use `Timeout.timeour` rather than `Time.now` to determine if
+      # waiting has exceeded timeout. Usage of `Time.now` is not compatible
+      # with Timecop gem - we may never exceed the timeout if it's stubbed.
+      #
+      # @param [Fixnum] timeout
+      # @yield block
+      # @api private
+      #
 
-  def wait(timeout, &block)
-    Timeout.timeout(timeout) do
-      (timeout / Watir::Wait::INTERVAL).to_i.times &block
-    end
-  rescue Timeout::Error
-    false
-  end
+      def wait(timeout, &block)
+        Timeout.timeout(timeout) do
+          (timeout / Watir::Wait::INTERVAL).to_i.times &block
+        end
+      rescue Timeout::Error
+        false
+      end
 
-end # Watir::Timecop::Timer
+    end # Timer
+  end # Timecop
+end # Watir
